8c2d38e3580fd04181118a9eec51a3527c5cae92
kangshinwon <wnfansl788@gmail.com>
2025-02-13 16:59:34 +0900

    improve model list UI and API Integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
@@ -8,19 +10,40 @@ use ratatui::{
     },
     DefaultTerminal,
 };
+use serde::{Deserialize, Serialize};
 
 struct App {
     exit: bool,
-    model_list: ModelList,
+    models_info: ModelInfo,
 }
 
-struct ModelList {
-    models: Vec<Model>,
+struct ModelInfo {
+    models: ModelList,
     selected_model: ListState,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct ModelList {
+    models: Vec<Model>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Model {
     name: String,
+    model: String,
+    modified_at: String,
+    size: u64,
+    digest: String,
+    details: ModelDetails,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ModelDetails {
+    format: String,
+    family: String,
+    families: Option<Vec<String>>,
+    parameter_size: String,
+    quantization_level: String,
 }
 
 impl Default for App {
@@ -28,15 +51,23 @@ impl Default for App {
         Self {
             exit: false,
             // TODO: load models from api
-            model_list: ModelList {
-                models: vec![
-                    Model {
+            models_info: ModelInfo {
+                models: ModelList {
+                    models: vec![Model {
                         name: "Model 1".to_string(),
-                    },
-                    Model {
-                        name: "Model 2".to_string(),
-                    },
-                ],
+                        model: "model1".to_string(),
+                        modified_at: "2021-01-01".to_string(),
+                        size: 1000,
+                        digest: "digest1".to_string(),
+                        details: ModelDetails {
+                            format: "gguf".to_string(),
+                            family: "llama".to_string(),
+                            families: None,
+                            parameter_size: "7B".to_string(),
+                            quantization_level: "Q4_0".to_string(),
+                        },
+                    }],
+                },
                 selected_model: ListState::default(),
             },
         }
@@ -62,8 +93,8 @@ impl App {
 
         match key.code {
             KeyCode::Char('q') => self.exit = true,
-            KeyCode::Down => self.model_list.selected_model.select_next(),
-            KeyCode::Up => self.model_list.selected_model.select_previous(),
+            KeyCode::Down => self.models_info.selected_model.select_next(),
+            KeyCode::Up => self.models_info.selected_model.select_previous(),
             KeyCode::Enter => {}
             _ => {}
         }
@@ -78,14 +109,14 @@ impl App {
 | (_) | | | (_| | | | | | | (_| |_____| |_| |_| | |_____| |  \__ \
  \___/|_|_|\__,_|_| |_| |_|\__,_|      \__|\__,_|_|     |_|  |___/",
         )
-        .bold()
         .centered()
         .render(area, buf);
     }
 
     fn render_model_list(&mut self, area: Rect, buf: &mut Buffer) {
         let items: Vec<ListItem> = self
-            .model_list
+            .models_info
+            .models
             .models
             .iter()
             .map(|model| ListItem::new(format!("{}", model.name)))
@@ -95,14 +126,19 @@ impl App {
             .highlight_symbol("> ")
             .highlight_spacing(HighlightSpacing::Always);
 
-        StatefulWidget::render(list, area, buf, &mut self.model_list.selected_model);
+        StatefulWidget::render(list, area, buf, &mut self.models_info.selected_model);
     }
 }
 
 impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let [header_area, list_area] =
-            Layout::vertical([Constraint::Length(8), Constraint::Fill(1)]).areas(area);
+            Layout::vertical([Constraint::Fill(1), Constraint::Fill(1)]).areas(area);
+
+        // TODO: length should be dynamic based on the number of models
+        let [header_area] = Layout::vertical([Constraint::Length(7)])
+            .flex(Flex::Center)
+            .areas(header_area);
 
         // TODO: length should be dynamic based on the number of models
         let [list_area] = Layout::horizontal([Constraint::Length(15)])
@@ -117,7 +153,20 @@ impl Widget for &mut App {
     }
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let res = reqwest::get("http://localhost:11434/api/tags").await;
+    match res {
+        Ok(res) => {
+            let body = res.bytes().await.unwrap();
+            let models = serde_json::from_slice::<ModelList>(&body)?;
+            println!("{:?}", models);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+
     let terminal = ratatui::init();
     let result = App::default().run(terminal);
     ratatui::restore();
